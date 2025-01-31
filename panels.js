dafc540a09449e610c629c059b87226be73c2eaf
callum <callumjosephiaingordon@gmail.com>
2025-01-31 18:16:54 +0100

    tried to add modularity but it didn't work

---
+++ panels.js
@@ -0,0 +1,20 @@
+const panels = [
+    { text: 'One', visible: false, p: 0 },
+    { text: 'Two', visible: false, p: 1 },
+    { text: 'Three', visible: false, p: 2 },
+    { text: 'Four', visible: false, p: 3 },
+    { text: 'Five', visible: false, p: 4 },
+    { text: 'Six', visible: false, p: 5 },
+    { text: 'Seven', visible: false, p: 6 },
+    { text: 'Eight', visible: false, p: 7 },
+    { text: 'Nine', visible: false, p: 8 },
+    { text: 'Ten', visible: false, p: 9 },
+    { text: 'Eleven', visible: false, p: 10 },
+    { text: 'Twelve', visible: false, p: 11 },
+    { text: 'Thirteen', visible: false, p: 12 },
+    { text: 'Fourteen', visible: false, p: 13 },
+    { text: 'Fifteen', visible: false, p: 14 },
+    { text: 'Sixteen', visible: false, p: 15 },
+  ];
+  
+  export { panels };
